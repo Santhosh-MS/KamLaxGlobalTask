d987cdbb19f3a5c18dbb05d553f6615e233d37bb
Git-Santhosh <https://github.com/Git-Santhosh/MySample.git>
2020-02-22 11:31:47 +0530

    Initial Commit

---
+++ KamLaxTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KamLaxTask
+//
+//  Created by Ducont on 22/02/20.
+//  Copyright © 2020 Ducont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
